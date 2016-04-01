4649cde15d7588a0302ade7d05ec7793379960bd
rhnvrm <rohanverma2004@gmail.com>
2016-04-01 15:50:44 +0530

    fixed class

--- mytime.py
+++ mytime.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 class Time(object):
 
     def __init__(self):
--- parsetime.py
+++ parsetime.py
@@ -1,86 +1,13 @@
 #import timex
-import mytime
+from mytime import Time
 import nltk
 import re
 
 
 from nltk import pos_tag, word_tokenize
-from datetime import datetime, timedelta
+
 from word2number import w2n
 
-class Time(object):
-
-    def __init__(self):
-        self.hours = int(datetime.now().strftime("%H"))
-        self.minutes = int(datetime.now().strftime("%M"))
-        self.year = int(datetime.now().strftime("%Y"))
-        self.month = datetime.now().strftime("%B")
-        self.imonth = int(datetime.now().strftime("%m"))
-        self.day = datetime.now().strftime("%A")
-        self.date = datetime.now().strftime("%d")
-
-    def update_hours(self, hrs):
-        self.hours += hrs
-        if(self.hours >= 24):
-            self.hours -= 24
-        time_temp = datetime.now() + timedelta(days=1)
-        self.set_date(time_temp.strftime("%d"))
-        self.update_day(time_temp)
-
-    def inc_date(self):
-        self.date = int(self.date) + 1
-        new_time = datetime(self.get_iyear(), self.get_imonth(), self.get_date() + 1)
-        self.update_day(new_time)
-
-    def update_min(self, min):
-        self.minutes += min
-        if(self.minutes >= 60):
-            self.minutes -= 60
-            self.update_hours(1)
-    
-    def update_day(self, time):
-        self.day = time.strftime("%A")
-
-    def set_hours(self, inp):
-        self.hours = inp
-    def set_min(self, inp):
-        self.minutes = inp
-    def set_year(self, inp):
-        self.year = inp
-    def set_month(self, inp):
-        self.month = inp
-    def set_day(self, inp):
-        self.day = inp
-    def set_date(self, inp):
-        self.date = inp
-
-    def get_hours(self):
-        if(self.hours >= 0 and self.hours <= 9):
-            return '0' + str(self.hours)
-        else:
-            return str(self.hours)
-    def get_ihours(self):
-        return self.hours
-    def get_min(self):
-        if(self.minutes >= 0 and self.minutes <=9):
-            return '0' + str(self.minutes)
-        return str(self.minutes)
-    def get_imin(self):
-        return self.minutes
-    def get_imonth(self):
-        return self.imonth
-    def get_year(self):
-        return str(self.year)
-    def get_iyear(self):
-        return self.year
-    def get_month(self):
-        return self.month
-    def get_day(self):
-        return self.day
-    def get_date(self):
-        return self.date
-    def get_sdate(self):
-        return str(self.date)
 
 types_of_sentence = {0:'[unknown]', 1:'[tStamp]', 2:'[tPeriod]', 3:'[tTrigger]'}
 
@@ -130,9 +57,7 @@ def parse_sentence(input):
     print(important_words)
 
 
-    print(types_of_sentence[sentence_type], time.get_hours() + time.get_min() + " hours, " + time.get_day() + ", " + time.get_date() + " " + time.get_month() + ", " + time.get_year()
-)
-
+    print(types_of_sentence[sentence_type], time.get_human())
 
 
 
